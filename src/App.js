7740bee814406389df000ecd44e2fa4f479d10c4
Dmitry <fantastico@live.ru>
2019-02-01 00:14:38 +0200

    fix(route): fix route for gh-pages

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ export default class App extends Component {
     render() {
         const { PUBLIC_URL } = process.env;
 
+        console.log(PUBLIC_URL);
+
         return (
-            <BrowserRouter basename={PUBLIC_URL}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Switch>
                     <Route exact path='/404' component={NotFound}/>
                     <Route path='/tasks/:id' component={Info}/>
