b0d30f4d8ea97752de6fde30f5b3b33564a24643
Dmitry <fantastico@live.ru>
2019-01-31 22:25:41 +0200

    fix(project): minor fixes (1)

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import NotFound from './components/NotFound';
 
 export default class App extends Component {
     render() {
+        console.log(process.env.PUBLIC_URL);
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route exact path={`${process.env.PUBLIC_URL}/404`} component={NotFound}/>
-                    <Route path={`${process.env.PUBLIC_URL}/tasks/:id`} component={Info}/>
-                    <Route path={`${process.env.PUBLIC_URL}/:tab?`} component={Main} />
+                    <Route exact path={`/${process.env.PUBLIC_URL}/404`} component={NotFound}/>
+                    <Route path={`/${process.env.PUBLIC_URL}/tasks/:id`} component={Info}/>
+                    <Route path={`/${process.env.PUBLIC_URL}/:tab?`} component={Main} />
                 </Switch>
             </BrowserRouter>
         );
