60d7fe6f132c9bfc75aefd0dd0b4045dafd0694d
Dmitry <fantastico@live.ru>
2019-02-01 00:28:21 +0200

    chore(project): publish changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import Main from './components/Main';
 import Info from './components/Info';
@@ -8,18 +7,13 @@ import NotFound from './components/NotFound';
 
 export default class App extends Component {
     render() {
-        const { PUBLIC_URL } = process.env;
-
-        console.log(PUBLIC_URL);
 
         return (
-            //{/*<BrowserRouter basename={process.env.PUBLIC_URL}>*/}
-                <Switch>
-                    <Route exact path='/404' component={NotFound}/>
-                    <Route path='/tasks/:id' component={Info}/>
-                    <Route path='/:tab?' component={Main} />
-                </Switch>
-            // </BrowserRouter>
+            <Switch>
+                <Route exact path='/404' component={NotFound}/>
+                <Route path='/tasks/:id' component={Info}/>
+                <Route path='/:tab?' component={Main} />
+            </Switch>
         );
     }
 }
